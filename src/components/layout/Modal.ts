4afcd0a9285dcd5f384c2d811993f3a2c5e60d3e
olhkyle <kylekwon.dev@gmail.com>
2025-03-03 19:12:07 +0900

    refactor : change structure of modal

--- src/components/layout/Modal.ts
+++ src/components/layout/Modal.ts
@@ -20,35 +20,44 @@ class Modal extends Component<ModalProps> {
 		return `${
 			isOpen
 				? `<div class="modal-container">
-						<button class="bx bx-x modal-close-btn"></button>
-						<div class="modal-header">
-							<i class='bx bx-window-alt'></i>
-							<div class="modal-card-title">
-								<textarea class="modal-card-title-textarea">${targetCard?.title}</textarea>
-								<div class="modal-card-list-item-title">in list <span href="#">${lists.find(list => list.id === listId)?.title}</span></div>
-							</div>
-						</div>
-						<div class="divider"></div>
-						<div class="modal-body">
-							<i class="bx bx-water"></i>
-							<div class="modal-card-content">
-								<div class="modal-card-content-header">
-									<div class="content-title-wrapper">
-										<div class="content-title">Description</div>
-										<span class="caution">Save your change 👉</span>
-									</div>
-									${
-										isCardDescCreatorOpen
-											? `<div class="description-control">
-													<button class="save-btn">Save</button>
-													<button class="bx bx-x description-close-btn"></button>
-												</div>`
-											: ''
-									}
+							<div class="modal-header">
+								<div class="modal-header-top">
+									<h2 class="modal-label">Change Content</h2>
+									<button class="bx bx-x modal-close-btn"></button>
 								</div>
-								<textarea class="modal-card-content-textarea" placeholder="Add a more detailed description">${targetCard?.description}</textarea>
+								<div class="modal-header-bottom">
+									<i class='bx bx-window-alt'></i>
+									<div class="modal-card-title">
+										<textarea class="modal-card-title-textarea">${targetCard?.title}</textarea>
+									</div>
+								</div>
+								<div class="modal-card-list-item-title">
+									📎
+									<span href="#">#${lists.find(list => list.id === listId)?.title}</span>
+								</div>
 							</div>
-						</div>
+							<div class="divider"></div>
+							<div class="modal-body">
+									<div class="modal-card-content-header">
+										<div class="content-title-group">
+											<i class="bx bx-water"></i>
+											<div class="content-title-wrapper">
+												<div class="content-title">Description</div>
+												<span class="caution">Save your change 👉</span>
+											</div>
+										</div>
+										${
+											isCardDescCreatorOpen
+												? `<div class="description-control">
+														<button class="save-btn">Save</button>
+														<button class="bx bx-x description-close-btn"></button>
+													</div>`
+												: ''
+										}
+									</div>
+									<textarea class="modal-card-content-textarea" placeholder="Add a more detailed description">${targetCard?.description}</textarea>
+								
+							</div>
 					</div>
 					<div class="overlay"></div>`
 				: ''
